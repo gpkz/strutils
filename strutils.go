fdd984e2fab64835c68aa4b075d353d2885ca97c
obj <48516500+obj@users.noreply.github.com>
2022-06-14 09:26:57 +0300

    add RemoveNonAlphaChars()

--- strutils.go
+++ strutils.go
@@ -1,4 +1,4 @@
-// Package strutils provides some simple string modifications
+//Package strutils provides some simple string modifications
 package strutils
 
 import "strings"
@@ -6,4 +6,16 @@ import "strings"
 // StandardizeSpaces replaces multiple spaces with a single space, also removing returns and tabs
 func StandardizeSpaces(s string) string {
 	return strings.Join(strings.Fields(s), " ")
+}
+
+// RemoveNonAlphaChars removes all non-alpha characters from the string (except for spaces)
+func RemoveNonAlphaChars(s string) string {
+	var result strings.Builder
+	for i := 0; i < len(s); i++ {
+		b := s[i]
+		if ('a' <= b && b <= 'z') || ('A' <= b && b <= 'Z') || b == ' ' {
+			result.WriteByte(b)
+		}
+	}
+	return result.String()
 }
--- strutils_test.go
+++ strutils_test.go
@@ -12,3 +12,12 @@ func TestStandardizeSpaces(t *testing.T) {
 	assert.Equal(t, "Hello, World !", StandardizeSpaces("  \n\n\nHello,  World  ! \r\n\r\r "))
 	assert.Equal(t, "Hello, World !", StandardizeSpaces("\tHello,   World  !  \r\t\n\n"))
 }
+
+func TestRemoveNonAlphaChars(t *testing.T) {
+	assert.Equal(t, "Hello World", RemoveNonAlphaChars("Hello, World!"))
+	assert.Equal(t, "Hello World", RemoveNonAlphaChars("Hello World!11!1"))
+	assert.Equal(t, "Hllo World", RemoveNonAlphaChars("H3llo World!"))
+	assert.Equal(t, "HelloWorld", RemoveNonAlphaChars("Hello\tWorld!"))
+	assert.Equal(t, "Hello World", RemoveNonAlphaChars("ĞHello WorldŞ!"))
+	assert.Equal(t, "Hello World", RemoveNonAlphaChars("01.Hello 02.WorldŞ!"))
+}
